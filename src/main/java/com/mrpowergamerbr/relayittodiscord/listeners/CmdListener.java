57bfe48be950471836fa9b6f946289177af126fe
AeSix <AeSix@users.noreply.github.com>
2018-05-05 04:54:57 -0400

    Added relayme command result stub
    This won't do much, but maybe it will return instructions for /relayme - if I've managed to do this correctly.  Probably not, and I might have written the key to unlock the gates and chains of C'thulhu, or maybe a galaxy sized Einstein-Rosen bridge.

--- src/main/java/com/mrpowergamerbr/relayittodiscord/listeners/CmdListener.java
+++ src/main/java/com/mrpowergamerbr/relayittodiscord/listeners/CmdListener.java
@@ -10,38 +10,47 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import com.mrpowergamerbr.relayittodiscord.RelayItToDiscord;
 import com.mrpowergamerbr.temmiewebhook.DiscordMessage;
 
-public class ChatListener implements Listener {
+public class CmdListener implements Listener {
 	RelayItToDiscord m;
 
-	public ChatListener(RelayItToDiscord m) {
+	public CmdListener(RelayItToDiscord m) {
 		this.m = m;
 		Bukkit.getPluginManager().registerEvents(this, m);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onChat(AsyncPlayerChatEvent e) {
-		if (m.startRelaying) {
-			if (m.getConfig().getBoolean("Advanced.IgnoreCancelled") && e.isCancelled()) {
-				return;
-			}
-			
-			String message = e.getMessage();
-			if (m.getConfig().getBoolean("StripColors")) {
-				// The super ultimate workaround(tm)
-				message = ChatColor.translateAlternateColorCodes('&', message);
-				message = ChatColor.stripColor(message);
-			}
-			String format = m.getConfig().getString("Format");
-			format = format.replace("%content%", message);
-			format = m.parse(format, e.getPlayer());
-			
-			DiscordMessage dm = DiscordMessage.builder()
-					.username(m.parse(m.getConfig().getString("Username"), e.getPlayer())) // Player's name
-					.content(format) // Player's message
-					.avatarUrl(m.parse(m.getConfig().getString("AvatarUrl"), e.getPlayer())) // Avatar
-					.build();
-			
-			m.temmie.sendMessage(dm);
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("relayme")) {
+			sender.SendMessage("You need your full Discord ID (@name#0002)");
+			sender.SendMessage("To confirm your identity to Discord, do:");
+			sender.SendMessage("/relayme @test#7479  - replacing the ID with your own!");
+			sender.SendMessage("This won't actually work until the rest of the code is written");
+			return true;
 		}
 	}
+#	public void onChat(AsyncPlayerChatEvent e) {
+#		if (m.startRelaying) {
+#			if (m.getConfig().getBoolean("Advanced.IgnoreCancelled") && e.isCancelled()) {
+#				return;
+#			}
+#			
+#			String message = e.getMessage();
+#			if (m.getConfig().getBoolean("StripColors")) {
+#				// The super ultimate workaround(tm)
+#				message = ChatColor.translateAlternateColorCodes('&', message);
+#				message = ChatColor.stripColor(message);
+#			}
+#			String format = m.getConfig().getString("Format");
+#			format = format.replace("%content%", message);
+#			format = m.parse(format, e.getPlayer());
+#			
+#			DiscordMessage dm = DiscordMessage.builder()
+#					.username(m.parse(m.getConfig().getString("Username"), e.getPlayer())) // Player's name
+#					.content(format) // Player's message
+#					.avatarUrl(m.parse(m.getConfig().getString("AvatarUrl"), e.getPlayer())) // Avatar
+#					.build();
+#			
+#			m.temmie.sendMessage(dm);
+#		}
+#	}
 }
